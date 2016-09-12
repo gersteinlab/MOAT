d7b2c2ae790faaeadd968367883771f5360ab3b5
Lucas Lochovsky <lucas.lochovsky@gmail.com>
2016-09-12 14:10:38 -0400

    Fixed an MPI_Bcast mismatch bug.

--- moatsim_mpi.cpp
+++ moatsim_mpi.cpp
@@ -1085,9 +1085,10 @@ int main (int argc, char* argv[]) {
 		
 		/* Signal child processes to end */
 		int permutation_flag = 0;
-		for (int j = 1; j < mpi_size; j++) {
-			MPI_Send(&permutation_flag, 1, MPI_INT, j, 8, MPI_COMM_WORLD);
-		}
+		MPI_Bcast(&permutation_flag, 1, MPI_INT, 0, MPI_COMM_WORLD);
+// 		for (int j = 1; j < mpi_size; j++) {
+// 			MPI_Send(&permutation_flag, 1, MPI_INT, j, 8, MPI_COMM_WORLD);
+// 		}
 		
 		// Wrap up by removing the temporary files created along the way
 		string rmcom = "rm " + regions_presig;
@@ -1521,7 +1522,7 @@ int main (int argc, char* argv[]) {
 				free(permuted_var_coor);
 			}
 			
-			MPI_Recv(&permutation_flag, 1, MPI_INT, 0, 8, MPI_COMM_WORLD, &status);
+			MPI_Bcast(&permutation_flag, 1, MPI_INT, 0, MPI_COMM_WORLD);
 			
 			// DEBUG
 			// printf("Breakpoint Tau\n");
