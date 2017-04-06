b260a6d8c81de9ac910aa14c587a67129e9a5f3e
Lucas Lochovsky <lucas.lochovsky@gmail.com>
2017-04-06 19:24:15 -0400

    Bug fix for retrieving wg signal p-values.

--- moat_a.cu
+++ moat_a.cu
@@ -798,7 +798,7 @@ int main (int argc, char* argv[]) {
 	vector<vector<string> > prohibited_regions;
 	
 	// DEBUG
-	printf("Breakpoint 1\n");
+	// printf("Breakpoint 1\n");
 	
 	// Bring variant file data into memory
 	// Save the first 3 columns, ignore the rest if there are any
@@ -903,7 +903,7 @@ int main (int argc, char* argv[]) {
 	}
 	
 	// DEBUG
-	printf("Breakpoint 2\n");
+	// printf("Breakpoint 2\n");
 	
 	// Sort the arrays
 	sort(var_array.begin(), var_array.end(), cmpIntervals);
@@ -953,7 +953,7 @@ int main (int argc, char* argv[]) {
 // 	for (unsigned int i = 0; i < var_array.size(); i++) {
 // 		printf("%s, %s, %s\n", var_array[i][0].c_str(), var_array[i][1].c_str(), var_array[i][2].c_str());
 // 	}
-	printf("Breakpoint 3\n");
+	// printf("Breakpoint 3\n");
 	
 	// Variables for main loop
 	// unsigned int var_pointer = 0;
@@ -1194,7 +1194,7 @@ int main (int argc, char* argv[]) {
 	// ann_array.clear();
 	
 	// DEBUG
-	printf("Breakpoint 4\n");
+	// printf("Breakpoint 4\n");
 	
 	// Begin the CUDA magic
 	int *gpu_var_chr;
@@ -1303,7 +1303,7 @@ int main (int argc, char* argv[]) {
 	// printf("This is debug print 1\n");
 	
 	// DEBUG
-	printf("Breakpoint 5\n");
+	// printf("Breakpoint 5\n");
 	
 	// Adjust the heap size based on the size of the dataset
 // 	if (ann_array.size() > 3000) {
@@ -1323,7 +1323,7 @@ int main (int argc, char* argv[]) {
 // 	printf("Test int: %d\n", *test_int_cpu);
 
 	// DEBUG
-	printf("Breakpoint 6\n");
+	// printf("Breakpoint 6\n");
 	
 	// GPUerrchk(cudaDeviceSynchronize());
 	
@@ -1335,7 +1335,7 @@ int main (int argc, char* argv[]) {
 	
 	double *signal_pvalues;
 	
-	if (funseq_opt) {
+	if (funseq_opt == 'p') {
 		signal_pvalues = (double *)malloc(ann_array.size()*sizeof(double));
 		cudaMemcpy(signal_pvalues, gpu_signal_pvalues, ann_arr_length*sizeof(double), cudaMemcpyDeviceToHost);
 	}
@@ -1377,7 +1377,7 @@ int main (int argc, char* argv[]) {
 // 	}
 
 	// DEBUG
-	printf("Breakpoint 7\n");
+	// printf("Breakpoint 7\n");
 
 	// Output generation
 	FILE *outfile_ptr = fopen(outfile.c_str(), "w");
