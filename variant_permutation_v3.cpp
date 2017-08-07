670be70241b6f15424263d8d456ce6ea1028bcb7
Lucas Lochovsky <lucas.lochovsky@gmail.com>
2017-08-07 14:20:01 -0400

    Small bug fix.

--- variant_permutation_v3.cpp
+++ variant_permutation_v3.cpp
@@ -222,7 +222,7 @@ pair<unsigned int,unsigned int> intersecting_variants (vector<vector<string> > c
 		printf("val_chr: %s\n", val_chr.c_str());
 		printf("val_start: %d\n", val_start_num);
 		printf("val_end: %d\n", val_end_num);
-		printf("reg_chr: %s\n", reg_chr.c_str());
+		printf("reg_chr: %s\n", region_chr.c_str());
 		printf("reg_end: %d\n", region_end_num);
 		
 		// Intersection test
